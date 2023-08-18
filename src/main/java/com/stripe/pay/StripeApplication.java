5e3eecc8ea9c64130120b7035d60422b5c847513
Teju <tejashwinitej27@gmail.com>
2023-08-18 17:35:18 +0530

    Update StripeApplication.java

--- src/main/java/com/stripe/pay/StripeApplication.java
+++ src/main/java/com/stripe/pay/StripeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StripeApplication {
+public class StripeApplication1 {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StripeApplication.class, args);
